670784c3f20d2b59f9bc73a924c6e9ff3f3e1a89
Vinayak Gadad <vinayak007gadad@gmail.com>
2018-12-19 14:42:59 -0800

    Learning python

---
+++ firsttest.py
@@ -0,0 +1,21 @@
+#1. print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are")
+
+#2. import sys
+# print(sys.version)
+# print(sys.version_info)
+# print(sys.api_version)
+
+#3. import datetime
+# print(datetime.datetime.now())
+# time = datetime.datetime.now()
+# print(time.strftime("%Y-%m-%d %T"))
+
+#4. from math import pi
+# radius = float(input("Enter the radius of circle :"))
+# print("The Area of circle with radius " + str(radius) + " is " + str(pi * radius**2))
+
+import os
+fname = str(input("Enter first name :"))
+lname = str(input("Enter the last Name :"))
+print(fname,lname)
+print(lname, fname)
